e3958bdf73b4bb9dd2056545afe6f26d68527a15
alexberry721 <alexberry721@gmail.com>
2016-10-08 21:59:15 -0700

    Initial Commit

---
+++ Book Exercise 5.51 - Check ISBN-13/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Book Exercise 5.51 - Check ISBN-13
+//
+//  Created by ax on 10/8/16.
+//  Copyright © 2016 COMP130. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
